707e8eb05a3ee216f5c547348b34b2e119881ce1
Harsh <thisisharshlakra@gmail.com>
2025-07-03 01:56:48 -0500

    Add files via upload

---
+++ showInput.js
@@ -0,0 +1,4 @@
+document.getElementById("showBtn").addEventListener("click", function() {
+  const inputText = document.getElementById("userInput").value;
+  document.getElementById("output").textContent = inputText;
+});
